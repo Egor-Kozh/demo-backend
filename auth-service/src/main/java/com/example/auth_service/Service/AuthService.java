2676a93e0aee26b69c58723df222469e8cc42e17
Egor-Kozh <kozhevnikovegor55@gmail.com>
2025-11-18 04:18:27 +0600

    feat: добавление исключений и их обработка

--- auth-service/src/main/java/com/example/auth_service/Service/AuthService.java
+++ auth-service/src/main/java/com/example/auth_service/Service/AuthService.java
@@ -1,14 +1,13 @@
 package com.example.auth_service.Service;
 
-import com.example.auth_service.Security.Model.JwtAuthenticationDto;
-import com.example.auth_service.Security.Model.RefreshTokenDto;
-import com.example.auth_service.Security.Model.UserCredentialsDto;
 import com.example.auth_service.Model.Dto.UserDto;
 import com.example.auth_service.Model.Entity.UserEntity;
 import com.example.auth_service.Model.Factory.UserDtoFactory;
 import com.example.auth_service.Model.Factory.UserEntityFactory;
 import com.example.auth_service.Repository.UserRepository;
 import com.example.auth_service.Security.Jwt.JwtService;
+import com.example.auth_service.Security.Model.RefreshTokenDto;
+import com.example.auth_service.Security.Model.UserCredentialsDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -30,43 +29,66 @@ public class AuthService {
 
 
     public ResponseEntity<String> createUser(UserDto newUser) {
+        try {
+            UserEntity user = userRepository.findUserByEmail(newUser.getEmail());
 
-        UserEntity createdUser = userEntityFactory.createUserEntity(newUser);
+            if (user != null) {
+                throw new RuntimeException("The user with this email already exists!");
+            }
 
-        userRepository.saveAndFlush(createdUser);
+            UserEntity createdUser = userEntityFactory.createUserEntity(newUser);
 
-        return ResponseEntity.ok().body("User was created");
-    }
+            userRepository.saveAndFlush(createdUser);
 
+            return ResponseEntity.ok().body("User was created");
 
-    public JwtAuthenticationDto singIn(UserCredentialsDto userCredentialsDto) {
-
-        UserEntity user = userRepository.findUserByEmail(userCredentialsDto.getEmail());
-
-        UserDto userDto = userDtoFactory.createUserDto(user);
-
-        if (passwordEncoder.matches(userCredentialsDto.getPassword(), user.getPassword())) {
-            return jwtService.generateAuthToken(userDto);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
-
-        return null;
     }
 
 
-    public JwtAuthenticationDto refreshToken(RefreshTokenDto refreshTokenDto) {
+    public ResponseEntity<?> signIn(UserCredentialsDto userCredentialsDto) {
+        try {
+            UserEntity user = userRepository.findUserByEmail(userCredentialsDto.getEmail());
 
-        String refreshToken = refreshTokenDto.getRefreshToken();
+            if (user == null) {
+                throw new RuntimeException("Invalid email or password!");
+            }
 
-        if (refreshToken != null && jwtService.validateJwtToken(refreshToken)) {
+            if (!passwordEncoder.matches(userCredentialsDto.getPassword(), user.getPassword())) {
+                throw new RuntimeException("Invalid email or password!");
+            }
+
+            UserDto userDto = userDtoFactory.createUserDto(user);
+
+            return ResponseEntity.ok().body(jwtService.generateAuthToken(userDto));
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+
+    public ResponseEntity<?> refreshToken(RefreshTokenDto refreshTokenDto) {
+        try {
+            String refreshToken = refreshTokenDto.getRefreshToken();
+
+            if (refreshToken == null) {
+                throw new RuntimeException("Invalid refresh token!");
+            }
+
+            if (!jwtService.validateJwtToken(refreshToken)) {
+                throw new RuntimeException("No valid refresh token!");
+            }
+
             UserEntity user = userRepository.findUserByEmail(jwtService.getEmailFromToken(refreshToken));
 
             UserDto userDto = userDtoFactory.createUserDto(user);
 
-            return jwtService.refreshBaseToken(userDto, refreshToken);
+            return ResponseEntity.ok().body(jwtService.refreshBaseToken(userDto, refreshToken));
+
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
-
-        return null;
     }
-
-
 }
