4e8ad6221ab01e4d171e1f458ee8c3a86585db73
Egor-Kozh <kozhevnikovegor55@gmail.com>
2025-11-19 21:38:54 +0600

    first commit

---
+++ type_product-service/src/test/java/com/example/type_product_service/TypeProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.type_product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypeProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
